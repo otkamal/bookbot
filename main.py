974139031ae21445b9b56c8af13f7f2a77293f06
otkamal <okamal@duck.com>
2025-01-20 21:27:08 -0800

    added report generator

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import string
 
+PATH_TO_BOOK = "books/frankenstein.txt"
+
 def count_characters(text: str):
     letter_count = dict.fromkeys(string.ascii_lowercase, 0)
     lower_text = text.lower()
     for c in lower_text:
-        if c in string.ascii_lowercase or c in letter_count.keys():
+        if c in string.ascii_lowercase:
             letter_count[c] += 1
-        else:
-            letter_count[c] = 1
     return letter_count
 
-with open("./books/frankenstein.txt") as f:
+def generate_report(file: str, num_words: int, char_counts: dict):
+    print(f"--- Begin report of {file} ---")
+    print(f"{num_words} words found in the document\n\n")
+    for c in char_counts.keys():
+        print(f"The '{c}' character was found {char_counts[c]} times")
+
+with open(PATH_TO_BOOK) as f:
     frankenstein_txt = f.read()
-    count = count_characters(frankenstein_txt)
-    print(count)
+    count_of_words = len(frankenstein_txt.split())
+    count_by_char = count_characters(frankenstein_txt)
+    generate_report(PATH_TO_BOOK, count_of_words, count_by_char)
     f.close()
 
