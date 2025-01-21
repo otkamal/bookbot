18a3dd308b0824bbdba5b1ea3ef5163f72209277
otkamal <okamal@duck.com>
2025-01-20 21:06:30 -0800

    changed feature to count characters

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import string
+
+def count_characters(text: str):
+    letter_count = dict.fromkeys(string.ascii_lowercase, 0)
+    lower_text = text.lower()
+    for c in lower_text:
+        if c in string.ascii_lowercase:
+            letter_count[c] += 1
+    return letter_count
+
 with open("./books/frankenstein.txt") as f:
     frankenstein_txt = f.read()
-    print(frankenstein_txt)
+    count = count_characters(frankenstein_txt)
+    print(count)
     f.close()
+
